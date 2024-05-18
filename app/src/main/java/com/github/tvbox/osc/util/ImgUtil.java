3dc498a7ff44617fef13f705024ff2005da7528d
zfengboy <144447286+zfengboy@users.noreply.github.com>
2024-05-18 20:10:03 +0800

    houfukude[fix]因图片错误导致详情页闪退&优化部分代码

--- app/src/main/java/com/github/tvbox/osc/util/ImgUtil.java
+++ app/src/main/java/com/github/tvbox/osc/util/ImgUtil.java
@@ -1,10 +1,13 @@
 package com.github.tvbox.osc.util;
 
 import static com.bumptech.glide.load.resource.bitmap.VideoDecoder.FRAME_OPTION;
+
 import android.graphics.Bitmap;
 import android.media.MediaMetadataRetriever;
 import android.text.TextUtils;
 import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import com.aminography.redirectglide.GlideApp;
@@ -25,8 +28,8 @@ import com.github.tvbox.osc.base.App;
 import com.google.common.net.HttpHeaders;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
+
 import java.io.UnsupportedEncodingException;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLDecoder;
 
@@ -69,62 +72,57 @@ public class ImgUtil {
         } else {
             if (roundingRadius == 0) roundingRadius = 1;
             RequestOptions requestOptions = new RequestOptions()
-                .format(DecodeFormat.PREFER_RGB_565)
-                .diskCacheStrategy(getDiskCacheStrategy(0))
-                .dontAnimate()
-                .transform(
-            new CenterCrop(),
-            new RoundedCorners(roundingRadius));
+                    .format(DecodeFormat.PREFER_RGB_565)
+                    .diskCacheStrategy(getDiskCacheStrategy(0))
+                    .dontAnimate()
+                    .transform(
+                            new CenterCrop(),
+                            new RoundedCorners(roundingRadius));
             GlideApp.with(App.getInstance())
-                .asBitmap()
-                .load(getUrl(url))
-                .error(R.drawable.img_loading_placeholder)
-                .placeholder(R.drawable.img_loading_placeholder)
-                .listener(getListener(view, ImageView.ScaleType.FIT_XY))
-                .apply(requestOptions)
-                .into(view);
+                    .asBitmap()
+                    .load(getUrl(url))
+                    .error(R.drawable.img_loading_placeholder)
+                    .placeholder(R.drawable.img_loading_placeholder)
+                    .listener(getListener(view, ImageView.ScaleType.FIT_XY))
+                    .apply(requestOptions)
+                    .into(view);
         }
     }
 
-    /*
+    /**
      * 使用Glide方式获取视频某一帧
      * @param uri 视频地址
      * @param imageView 设置image
      * @param frameTimeMicros 获取某一时间帧.
-     */
+     **/
     public static void loadVideoScreenshot(String uri, ImageView imageView, long frameTimeMicros) {
         RequestOptions requestOptions = RequestOptions.frameOf(frameTimeMicros * 1000)
-            .set(FRAME_OPTION, MediaMetadataRetriever.OPTION_CLOSEST)
-            .transform(
-        new CenterCrop(),
-        new RoundedCorners(10));
+                .set(FRAME_OPTION, MediaMetadataRetriever.OPTION_CLOSEST)
+                .transform(
+                        new CenterCrop(),
+                        new RoundedCorners(10));
         Glide.with(App.getInstance())
-            .load(uri)
-            .skipMemoryCache(true)
-            .apply(requestOptions)
-            .into(imageView);
+                .load(uri)
+                .skipMemoryCache(true)
+                .apply(requestOptions)
+                .into(imageView);
     }
 
     public static String getDiskCacheStrategyName(int index) {
-        String[] names = new String[] {
-            "[NONE] 关闭", "[RESOURCE] 转换图片", "[DATA] 原始图片 ", "[ALL] 原始图片和转换图片", "[AUTOMATIC] 自动"
+        String[] names = new String[]{
+                "[NONE] 关闭", "[RESOURCE] 转换图片", "[DATA] 原始图片 ", "[ALL] 原始图片和转换图片", "[AUTOMATIC] 自动"
         };
         return names[index];
     }
 
     public static DiskCacheStrategy getDiskCacheStrategy(int index) {
-        switch (index) {
-            case 1:
-                return DiskCacheStrategy.RESOURCE;
-            case 2:
-                return DiskCacheStrategy.DATA;
-            case 3:
-                return DiskCacheStrategy.ALL;
-            case 4:
-                return DiskCacheStrategy.AUTOMATIC;
-            default:
-                return DiskCacheStrategy.NONE;
-        }
+        return switch (index) {
+            case 1 -> DiskCacheStrategy.RESOURCE;
+            case 2 -> DiskCacheStrategy.DATA;
+            case 3 -> DiskCacheStrategy.ALL;
+            case 4 -> DiskCacheStrategy.AUTOMATIC;
+            default -> DiskCacheStrategy.NONE;
+        };
     }
 
     private static Object getUrl(String url) {
@@ -146,7 +144,7 @@ public class ImgUtil {
             try {
                 header = URLDecoder.decode(header, "UTF-8");
             } catch (UnsupportedEncodingException e) {
-                e.printStackTrace();
+                LOG.e(e);
             }
         }
         if (url.contains("@Cookie=")) cookie = url.split("@Cookie=")[1].split("@")[0];
@@ -165,10 +163,10 @@ public class ImgUtil {
 
         if (!TextUtils.isEmpty(header)) {
             JsonObject jsonInfo = new Gson()
-                .fromJson(header, JsonObject.class);
-            for (String key: jsonInfo.keySet()) {
+                    .fromJson(header, JsonObject.class);
+            for (String key : jsonInfo.keySet()) {
                 String val = jsonInfo.get(key)
-                    .getAsString();
+                        .getAsString();
                 builder.addHeader(key, val);
             }
         } else {
@@ -188,23 +186,25 @@ public class ImgUtil {
             URL imgUrl = new URL(url);
             String host = imgUrl.getHost();
             builder.addHeader(HttpHeaders.HOST, host);
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
+            // GlideUrl 也可能为空 导致闪退
+            return new GlideUrl(url, builder.build());
+        } catch (Exception e) {
+            LOG.e(e);
+            return url;
         }
-
-        return new GlideUrl(url, builder.build());
     }
 
-    private static RequestListener < Bitmap > getListener(ImageView view, ImageView.ScaleType scaleType) {
-        return new RequestListener < Bitmap > () {@Override
-            public boolean onLoadFailed(@Nullable GlideException e, Object model, Target < Bitmap > target, boolean isFirstResource) {
+    private static RequestListener<Bitmap> getListener(ImageView view, ImageView.ScaleType scaleType) {
+        return new RequestListener<>() {
+            @Override
+            public boolean onLoadFailed(@Nullable GlideException e, Object model, @NonNull Target<Bitmap> target, boolean isFirstResource) {
                 view.setScaleType(scaleType);
                 view.setImageResource(R.drawable.img_loading_placeholder);
                 return true;
             }
 
             @Override
-            public boolean onResourceReady(Bitmap resource, Object model, Target < Bitmap > target, DataSource dataSource, boolean isFirstResource) {
+            public boolean onResourceReady(@NonNull Bitmap resource, @NonNull Object model, Target<Bitmap> target, @NonNull DataSource dataSource, boolean isFirstResource) {
                 view.setScaleType(scaleType);
                 return false;
             }
